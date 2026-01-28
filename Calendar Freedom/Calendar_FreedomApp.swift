2d3b11df7ddda6682347f2dd3000dbe7917b933b
Mirko Swillus <mechko@mk0.eu>
2026-01-28 10:21:16 +0100

    Initial Commit

---
+++ Calendar Freedom/Calendar_FreedomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar_FreedomApp.swift
+//  Calendar Freedom
+//
+//  Created by Mirko Swillus on 28.01.26.
+//
+
+import SwiftUI
+
+@main
+struct Calendar_FreedomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
